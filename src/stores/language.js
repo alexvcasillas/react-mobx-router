9a6712e371815b777e67f59e96ef936ea1efed95
Alex Casillas <alexvcasillas@gmail.com>
2017-03-13 09:25:34 +0100

    Updated LanguageStore to achieve a better performance with @computed instead of @observable for resource object

--- src/stores/language.js
+++ src/stores/language.js
@@ -5,43 +5,28 @@ import enUS from '../language/i18n/en';
 
 export default class LanguageStore {
   @observable language;
-  @observable resource;
+
+  @computed get resource() {
+    switch (this.language) {
+      case 'en':
+        return enUS;
+        break;
+      case 'es':
+        return esES;
+        break;
+      default:
+        return enUS;
+        break;
+    }
+  }
 
   constructor(language) {
     // Set the Initial State of Language Observable
     this.language = language;
-    // Set the Initial State of Resource Observable
-    switch (this.language) {
-      case 'en':
-        this.resource = enUS;
-        break;
-      case 'es':
-        this.resource = esES;
-        break;
-      default:
-        this.resource = enUS;
-        break;
-    }
-    // Stablish a Reaction for changing Resources on the fly when Language Observer Changes
-    reaction(
-      () => this.language,
-      () => {
-        switch (this.language) {
-          case 'en':
-            this.resource = enUS;
-            break;
-          case 'es':
-            this.resource = esES;
-            break;
-          default:
-            this.resource = enUS;
-            break;
-        }
-      }
-    );
   }
 
-  @action("Change language to") changeLanguageTo(language) {
+  @action('Change language to')
+  changeLanguageTo(language) {
     this.language = language;
   }
 

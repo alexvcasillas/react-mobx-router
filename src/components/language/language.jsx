2eef46d9415e8b921f9229d4a0da5e2a135669e5
Alejandro Luis Vazquez Casillas <alexcasillas@MacBook-Pro-de-Alejandro.local>
2017-04-25 21:24:00 +0200

    Decoupled State from UI. Improved performance on i18n

--- src/components/language/language.jsx
+++ src/components/language/language.jsx
@@ -8,25 +8,3 @@ const Language = ({ language, resource }) => resource.indexOf('.') !== -1
   : <span>{language.resource[resource]}</span>;
 
 export default inject('language')(observer(Language));
-
-// @inject('language')
-// @observer
-// class Language extends React.Component {
-//   render() {
-//     const { language, resource } = this.props;
-//     const isNestedResource = resource.indexOf('.') !== -1;
-//     return (
-//       <span>
-//         {isNestedResource
-//           ? language.resource[resource.split('.')[0]][resource.split('.')[1]]
-//           : language.resource[resource]}
-//       </span>
-//     );
-//   }
-// }
-//
-// Language.propTypes = {
-//   resource: PropTypes.string
-// };
-//
-// export default Language;

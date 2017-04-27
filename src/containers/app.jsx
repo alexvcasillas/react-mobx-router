bf605b9e136a14ebc4fa4b2de5658ef93b5b8ede
Alex Casillas <alexvcasillas@gmail.com>
2017-04-27 12:40:32 +0200

    Quick Hotfix!
    Removed useless imports that I forgot to remove in the previous update.

--- src/containers/app.jsx
+++ src/containers/app.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { observer, inject } from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
 
-import Language from '../components/language/language';
 import Header from '../components/header/header';
 import User from '../components/user/user';
 

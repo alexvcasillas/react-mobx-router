aa3a2b13435082ccbe51dce3efd3e9df686eada8
Alex Casillas <alexvcasillas@gmail.com>
2017-04-27 14:14:43 +0200

    🔥 Quick Hotfix - Fixed user props
    Fixed a little issue with user deconstructed props :)
    Now everything works smoothly as expected 👌

--- src/components/user/user.jsx
+++ src/components/user/user.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { observer, inject } from 'mobx-react';
 
 import Language from '../language/language';
@@ -6,42 +6,42 @@ import Language from '../language/language';
 // Stylesheet Imports
 import './stylesheets/user.scss';
 
-const User = ({user, fullName, age, xp}) => (
+const User = ({ user }) => (
   <div id="user">
-        <div className="container">
-          <div className="card">
-            <div className="card-block">
-              <h4 className="card-title">
-                {fullName}, <small>{age}</small>
-              </h4>
-              <h6 className="card-subtitle mb-2 text-muted">
-                <Language resource="XP_TITLE" />: {xp}
-              </h6>
-              <p className="card-text">
-                <Language resource="APP_DESCRIPTION" />
-              </p>
-              <button
-                className="d-block btn btn-primary"
-                onClick={() => user.increaseXp(100)}
-              >
-                <Language resource="ACTIONS.INCREASE_XP" />
-              </button>
-              <button
-                className="d-block btn btn-secondary"
-                onClick={() => user.changeName('John')}
-              >
-                <Language resource="ACTIONS.CHANGE_NAME" />
-              </button>
-              <button
-                className="d-block btn btn-secondary"
-                onClick={() => user.changeLastName('Doe')}
-              >
-                <Language resource="ACTIONS.CHANGE_LASTNAME" />
-              </button>
-            </div>
-          </div>
+    <div className="container">
+      <div className="card">
+        <div className="card-block">
+          <h4 className="card-title">
+            {user.fullName}, <small>{user.age}</small>
+          </h4>
+          <h6 className="card-subtitle mb-2 text-muted">
+            <Language resource="XP_TITLE" />: {user.xp}
+          </h6>
+          <p className="card-text">
+            <Language resource="APP_DESCRIPTION" />
+          </p>
+          <button
+            className="d-block btn btn-primary"
+            onClick={() => user.increaseXp(100)}
+          >
+            <Language resource="ACTIONS.INCREASE_XP" />
+          </button>
+          <button
+            className="d-block btn btn-secondary"
+            onClick={() => user.changeName('John')}
+          >
+            <Language resource="ACTIONS.CHANGE_NAME" />
+          </button>
+          <button
+            className="d-block btn btn-secondary"
+            onClick={() => user.changeLastName('Doe')}
+          >
+            <Language resource="ACTIONS.CHANGE_LASTNAME" />
+          </button>
         </div>
       </div>
-)
+    </div>
+  </div>
+);
 
 export default inject('user')(observer(User));

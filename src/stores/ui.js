4b80d4a2a5dc1a989ec8b7050c64d2b4a03f62a8
Alex Casillas <alexvcasillas@gmail.com>
2017-05-12 14:48:29 +0200

    Removed some old code comments at UIStore

--- src/stores/ui.js
+++ src/stores/ui.js
@@ -17,24 +17,3 @@ const UIStore = types.model(
 );
 
 export default UIStore;
-
-// import { observable, action, computed } from 'mobx';
-//
-// export default class UIStore {
-//   @observable borderRadius;
-//   @observable textColor;
-//
-//   constructor(br, tc) {
-//     this.borderRadius = br;
-//     this.textColor = tc;
-//   }
-//
-//   @action(`Changin' Border Radius`)
-//   changeBorderRadius(value) {
-//     this.borderRadius = value;
-//   }
-//
-//   @computed get computeBorderRadius() {
-//     return this.borderRadius;
-//   }
-// }

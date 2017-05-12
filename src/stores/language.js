de5328f33a1aef70c0b28d4473b36eaaeb5d0949
Alex Casillas <alexvcasillas@gmail.com>
2017-05-12 14:47:57 +0200

    Removed some old code comments at LanguageStore

--- src/stores/language.js
+++ src/stores/language.js
@@ -29,34 +29,3 @@ const LanguageStore = types.model(
 );
 
 export default LanguageStore;
-
-// import { observable, computed, action } from 'mobx';
-//
-// export default class LanguageStore {
-//   @observable language;
-//
-//   @computed get resource() {
-//     switch (this.language) {
-//       case 'en':
-//         return enUS;
-//       case 'es':
-//         return esES;
-//       default:
-//         return enUS;
-//     }
-//   }
-//
-//   constructor(language) {
-//     // Set the Initial State of Language Observable
-//     this.language = language;
-//   }
-//
-//   @action('Change language to')
-//   changeLanguageTo(language) {
-//     this.language = language;
-//   }
-//
-//   @computed get currentLanguage() {
-//     return this.language;
-//   }
-// }
